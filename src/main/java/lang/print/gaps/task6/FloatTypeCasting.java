11ed80203fdaa6eb589a92193e021f70f5c8526d
Sophia <116626295+sofiyakamalova@users.noreply.github.com>
2023-02-11 17:20:34 +0600

    done

--- src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
@@ -2,5 +2,10 @@ package lang.print.gaps.task6;
 
 public class FloatTypeCasting {
     public void roundNumber(float numberToBeRounded) {
+        if (numberToBeRounded-(int)numberToBeRounded<0.5){
+            System.out.println((int) numberToBeRounded);
+        }else{
+            System.out.println((int)numberToBeRounded+1);
+        }
     }
 }
--- src/main/java/lang/print/gaps/task6/IntegerTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/IntegerTypeCasting.java
@@ -2,5 +2,8 @@ package lang.print.gaps.task6;
 
 public class IntegerTypeCasting {
     public static void main(String[] args) {
+        long first = 1000L;
+        int second = (int) first;
+        System.out.println(first + "\n" + second);
     }
 }
